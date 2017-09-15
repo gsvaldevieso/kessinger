2720cb520bc21e9635d2a4f5d281064eacb639a9
Guilherme Valdevieso <gsvaldevieso@gmail.com>
2017-09-15 20:00:29 -0300

    adicao dos campos na migration

--- database/migrations/2017_09_15_224921_create_profiles_table.php
+++ database/migrations/2017_09_15_224921_create_profiles_table.php
@@ -14,6 +14,18 @@ class CreateProfilesTable extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
+            $table->string('full_name', 200);
+            $table->string('address', 100);
+            $table->string('cep', 10);
+            $table->string('grade', 100);
+            $table->string('area', 100);
+            $table->integer('address_number');
+            $table->string('cpf', 20);
+            $table->string('nacionalidade');
+            $table->integer('user_id');
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
             $table->increments('id');
             $table->timestamps();
         });
