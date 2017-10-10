1d37412822671c6a99b68a9dc13de019a576e7bf
Guilherme Soares Valdevieso <gvaldevieso@localhost.localdomain>
2017-10-09 22:24:37 -0300

    ajustando formulario

--- resources/views/periodicos/create.blade.php
+++ resources/views/periodicos/create.blade.php
@@ -1,54 +1,61 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-	<div class="row">
-            <form method="POST" action="/periodicos" enctype="multipart/form-data" class="col s12">
-            {{ csrf_field() }}
-            <div class="row">
-                <div class="input-field col s6">
-                <input placeholder="Placeholder" name="titulo" id="titulo" type="text" class="validate">
-                <label for="titulo">Título</label>
-                </div>
-                <div class="input-field col s6">
-                <input name="issn" id="issn" type="text" class="validate">
-                <label for="issn">ISSN</label>
-                </div>
+    <div class="row">
+        <div class="panel panel-info">
+            <div style="background-color:white;" class="panel-heading">
+                <h3 class="panel-title">Criar novo periódico</h3>
             </div>
-            <div class="row">
-                <div class="input-field col s6">
-                <input placeholder="Placeholder" name="area_atuacao" id="area_atuacao" type="text" class="validate">
-                <label for="area_atuacao">Área de atuação</label>
-                </div>
-                <div class="input-field col s3">
-                <input id="fator_impacto" name="fator_impacto" type="text" class="validate">
-                <label for="fator_impacto">Fator de impacto</label>
-                </div>
-                <div class="input-field col s3">
-                <input placeholder="Placeholder" name="qualis" id="qualis" type="text" class="validate">
-                <label for="qualis">Qualis</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                <textarea id="descricao" name="descricao" class="materialize-textarea"></textarea>
-                <label for="descricao">Descrição</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="file-field input-field">
-                    <div class="btn">
-                        <span>Anexar imagem</span>
-                        <input name="imagem" type="file" multiple>
+            <div class="panel-body">
+                <form method="POST" action="/periodicos" enctype="multipart/form-data" class="col s12">
+                    {{ csrf_field() }}
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input placeholder="Placeholder" name="titulo" id="titulo" type="text" class="validate">
+                            <label for="titulo">Título</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input name="issn" id="issn" type="text" class="validate">
+                            <label for="issn">ISSN</label>
+                        </div>
                     </div>
-                    <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Selecione uma imagem que descreva o periódico...">
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input placeholder="Placeholder" name="area_atuacao" id="area_atuacao" type="text" class="validate">
+                            <label for="area_atuacao">Área de atuação</label>
+                        </div>
+                        <div class="input-field col s3">
+                            <input id="fator_impacto" name="fator_impacto" type="text" class="validate">
+                            <label for="fator_impacto">Fator de impacto</label>
+                        </div>
+                        <div class="input-field col s3">
+                            <input placeholder="Placeholder" name="qualis" id="qualis" type="text" class="validate">
+                            <label for="qualis">Qualis</label>
+                        </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea id="descricao" name="descricao" class="materialize-textarea"></textarea>
+                            <label for="descricao">Descrição</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>Anexar imagem</span>
+                                <input name="imagem" type="file" multiple>
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text" placeholder="Selecione uma imagem que descreva o periódico...">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <input class="waves-effect waves-light btn" type="submit" value="Submeter" />
+                    </div>
+                </form>
             </div>
-            <div class="row">
-                <input class="waves-effect waves-light btn" type="submit" value="Submeter" />
-            </div>  
-            </form>
         </div>
+    </div>
 </div>
-@stop 
+@stop
--- resources/views/publicacoes/create.blade.php
+++ resources/views/publicacoes/create.blade.php
@@ -1,65 +1,73 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-	<div class="row">
-            <form method="POST" action="/publicacoes" enctype="multipart/form-data" class="col s12">
-            {{ csrf_field() }}
-            <div class="row">
-                <div class="input-field col s6">
-                <input placeholder="Placeholder" name="autores" id="autores" type="text" class="validate">
-                <label for="autores">Autores</label>
-                </div>
-                <div class="input-field col s6">
-                <input name="titulo" id="titulo" type="text" class="validate">
-                <label for="titulo">Título</label>
-                </div>
+    <div class="row">
+        <div class="panel panel-info">
+            <div style="background-color:white;" class="panel-heading">
+                <h3 class="panel-title">Criar nova publicação</h3>
             </div>
-            <div class="row">
-                <div class="input-field col s6">
-                <input name="ano" placeholder="Placeholder" id="ano" type="text" class="validate">
-                <label for="ano">Ano da publicação</label>
-                </div>
-                <div class="input-field col s6">
-                <input name="area_atuacao" id="area_atuacao" type="text" class="validate">
-                <label for="area_atuacao">Área de atuação</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                <input name="categoria" placeholder="Placeholder" id="categoria" type="text" class="validate">
-                <label for="categoria">Categoria</label>
-                </div>
-                <div class="input-field col s6">
-                <select name="periodico" id="periodico">
-                    @foreach($periodicos as $periodico)
-                        <option value="{{$periodico->id}}">{{$periodico->titulo}}</option>
-                    @endforeach
-                </select>
-                <label>Periódico</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="file-field input-field">
-                    <div class="btn">
-                        <span>Publicação</span>
-                        <input name="publicacao" type="file" multiple>
+            <div class="panel-body">
+                <form method="POST" action="/publicacoes" enctype="multipart/form-data" class="col s12">
+                    {{ csrf_field() }}
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input placeholder="Placeholder" name="autores" id="autores" type="text" class="validate">
+                            <label for="autores">Autores</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input name="titulo" id="titulo" type="text" class="validate">
+                            <label for="titulo">Título</label>
+                        </div>
                     </div>
-                    <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Selecione uma imagem que descreva o periódico...">
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input name="ano" placeholder="Placeholder" id="ano" type="text" class="validate">
+                            <label for="ano">Ano da publicação</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input name="area_atuacao" id="area_atuacao" type="text" class="validate">
+                            <label for="area_atuacao">Área de atuação</label>
+                        </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input name="categoria" placeholder="Placeholder" id="categoria" type="text" class="validate">
+                            <label for="categoria">Categoria</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <select name="periodico" id="periodico">
+                                @foreach($periodicos as $periodico)
+                                <option value="{{$periodico->id}}">{{$periodico->titulo}}</option>
+                                @endforeach
+                            </select>
+                            <label>Periódico</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>Publicação</span>
+                                <input name="publicacao" type="file" multiple>
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text" placeholder="Selecione uma imagem que descreva o periódico...">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <input class="waves-effect waves-light btn" type="submit" value="Submeter" />
+                    </div>
+                </form>
+                
             </div>
-            <div class="row">
-                <input class="waves-effect waves-light btn" type="submit" value="Submeter" />
-            </div>  
-            </form>
         </div>
+    </div>
 </div>
-  <script type="text/javascript">
-  	
-  $(document).ready(function() {
-    $('select').material_select();
-  });
-     
-  </script>
-@stop 
+<script type="text/javascript">
+    
+$(document).ready(function() {
+$('select').material_select();
+});
+
+</script>
+@stop
