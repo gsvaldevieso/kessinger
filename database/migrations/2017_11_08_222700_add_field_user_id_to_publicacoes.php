79cfba33d48515b816e99760c29878284c712ca5
TamiGanzert <tami.g.bsp@gmail.com>
2017-11-13 20:36:58 -0200

    RF005 - Cadastro de publicações

---
+++ database/migrations/2017_11_08_222700_add_field_user_id_to_publicacoes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldUserIdToPublicacoes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('publicacoes', function(Blueprint $table) {
+            $table->integer('user_id')->default(0);
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('publicacoes', function(Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
