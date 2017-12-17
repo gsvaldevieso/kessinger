a6a2a307da43d576ad623a5fe58088ce88b0676f
TamiGanzert <tami.g.bsp@gmail.com>
2017-12-17 11:42:31 -0200

    Teste Tamires

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    static function validarCep($cep) {
+	    // retira espacos em branco
+	    $cep = trim($cep);
+	    
+	    // expressao regular para avaliar o cep
+	    $avaliaCep = preg_match("/[0-9]{5}-[0-9]{3}/", $cep);
+	    
+	    // verifica o resultado
+	    return (bool)$avaliaCep;
+	}
+
+	public static function validateName($name){
+    	$name = trim($name);
+    	$regex  = "/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/";
+    	
+    	return (bool)(preg_match($regex, $name));
+	}
 }
